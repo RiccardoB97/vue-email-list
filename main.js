326fe4bdc9238c8aaf3e773e51265dd3f0a337ed
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-27 13:53:42 +0200

    Use Axios to print a list of 10 random emails

---
+++ main.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+    el: "#app",
+
+    data: {
+        emailList: [],
+    },
+
+    methods: {
+
+    },
+
+    mounted() {
+        for (var i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(response => {
+                    this.emailList.push(response.data.response)
+                })
+        }
+    }
+
+})
